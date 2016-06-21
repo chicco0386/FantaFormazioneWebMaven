c41a59107d5746c5e699c150b1ed4db6f61fd846
enrico <enrico@ic1400833>
2016-06-21 12:56:19 +0200

    Vado avanti con la chiamate ad ejb da seam

--- src/main/java/it/zeze/fantaformazioneweb/session/SquadreList.java
+++ src/main/java/it/zeze/fantaformazioneweb/session/SquadreList.java
@@ -1,33 +1,17 @@
 package it.zeze.fantaformazioneweb.session;
 
-import it.zeze.fantaformazioneweb.entity.Squadre;
-import it.zeze.html.cleaner.HtmlCleanerUtil;
-import it.zeze.util.ConfigurationUtil;
-import it.zeze.util.Constants;
-
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.xpath.XPathExpressionException;
+import javax.naming.NamingException;
 
-import org.htmlcleaner.TagNode;
-import org.htmlcleaner.XPatherException;
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.framework.EntityQuery;
 import org.jboss.seam.log.Log;
+
+import it.zeze.fanta.ejb.util.JNDIUtils;
+import it.zeze.fanta.service.definition.ejb.proxy.seam.SquadreSeamRemote;
+import it.zeze.fantaformazioneweb.entity.Squadre;
 
 @Name("squadreList")
 public class SquadreList extends EntityQuery<Squadre> {
@@ -37,20 +21,27 @@ public class SquadreList extends EntityQuery<Squadre> {
 	@Logger
 	static Log log;
 
-	@In(create = true)
-	SquadreHome squadreHome;
-
-	private static final String EJBQL = "select squadre from Squadre squadre";
-
-	private static final String QUERY_GET_SQUADRA_BY_NAME = "select squadre from Squadre squadre where squadre.nome=:nomeSquadra";
-	private static final String QUERY_GET_SQUADRA_BY_ID = "select squadre from Squadre squadre where squadre.id=:id";	
+	private static final String EJBQL = "select squadre from Squadre squadre";	
 
 	private static final String[] RESTRICTIONS = { "lower(squadre.nome) like lower(concat(#{squadreList.squadre.nome},'%'))", };
-
+	
 	private Squadre squadre = new Squadre();
-
-	private Map<Integer, String> mapSquadre = new HashMap<Integer, String>();
-
+	
+	public Squadre getSquadre() {
+		return squadre;
+	}
+	
+	private static SquadreSeamRemote squadreEJB;
+		
+	static {
+		try {
+			squadreEJB = JNDIUtils.getSquadreEJB();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public SquadreList() {
 		setEjbql(EJBQL);
 		setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS));
@@ -59,122 +50,24 @@ public class SquadreList extends EntityQuery<Squadre> {
 		// setMaxResults(25);
 	}
 
-	public Squadre getSquadre() {
-		return squadre;
-	}
-
 	public void unmarshallAndSaveFromHtmlFile() {
-		log.info("unmarshallAndSaveFromHtmlFile, entrato");
-		String rootHTMLFiles = ConfigurationUtil.getValue(Constants.CONF_KEY_HTML_ROOT);
-		String nomeFileSquadre = ConfigurationUtil.getValue(Constants.CONF_KEY_HTML_FILE_SQUADRE);
-		String pathCompletoFileSquadre = rootHTMLFiles + nomeFileSquadre;
-		log.debug("Leggo il file HTML [" + pathCompletoFileSquadre + "]");
-		List<TagNode> listNodeSquadre;
-		try {
-			listNodeSquadre = HtmlCleanerUtil.getListOfElementsByXPathFromFile(pathCompletoFileSquadre, "//div[@class='content']/table/tbody/tr/td[@class='a-left']/a");
-			if (listNodeSquadre == null || listNodeSquadre.isEmpty()) {
-				// Leggo squadre nuovo HTML
-				listNodeSquadre = HtmlCleanerUtil.getListOfElementsByXPathSpecialFromFile(pathCompletoFileSquadre, "//table[@id='DataTables_Table_0']/tbody/tr/td/a/span[contains(@class,'nteam')][1]");
-			}
-			TagNode currentNodeSquadra;
-			String nomeSquadra;
-			Squadre foundSquadra = null;
-			for (int i = 0; i < listNodeSquadre.size(); i++) {
-				currentNodeSquadra = listNodeSquadre.get(i);
-				nomeSquadra = currentNodeSquadra.getText().toString().trim();
-				if (!nomeSquadra.isEmpty()) {
-					foundSquadra = getSquadraByNome(nomeSquadra);
-					if (foundSquadra != null && foundSquadra.getId() > 0) {
-						log.info("Squadra [" + nomeSquadra + "] gia' inserita");
-					} else {
-						squadreHome.clearInstance();
-						squadreHome.getInstance().setNome(nomeSquadra.toUpperCase());
-						squadreHome.persist();
-					}
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (XPatherException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (XPathExpressionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TransformerFactoryConfigurationError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TransformerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		log.info("unmarshallAndSaveFromHtmlFile, uscito");
+		squadreEJB.unmarshallAndSaveFromHtmlFile();
 	}
 
 	public void initMappaSquadre() {
-		mapSquadre.clear();
-		List<Squadre> result = this.getResultList();
-		for (int i = 0; i < result.size(); i++) {
-			mapSquadre.put(result.get(i).getId(), result.get(i).getNome());
-		}
+		squadreEJB.initMappaSquadre();
 	}
 
 	public Squadre getSquadraFromMapByNome(String nomeSquadraToSearch) {
-		Squadre squadraToReturn = new Squadre();
-		if (mapSquadre == null || mapSquadre.isEmpty()) {
-			initMappaSquadre();
-		}
-		Iterator<Entry<Integer, String>> it = mapSquadre.entrySet().iterator();
-		boolean trovato = false;
-		Entry<Integer, String> currentEntity;
-		while (it.hasNext() && !trovato) {
-			currentEntity = it.next();
-			if (currentEntity.getValue().equalsIgnoreCase(nomeSquadraToSearch.trim())) {
-				trovato = true;
-				squadraToReturn.setId(currentEntity.getKey());
-				squadraToReturn.setNome(currentEntity.getValue());
-			}
-		}
-		// Ricerco per LIKE per nuovo HTML
-		if (!trovato){
-			it = mapSquadre.entrySet().iterator();
-			while (it.hasNext() && !trovato) {
-				currentEntity = it.next();
-				if (currentEntity.getValue().toLowerCase().startsWith(nomeSquadraToSearch.trim().toLowerCase())) {
-					trovato = true;
-					squadraToReturn.setId(currentEntity.getKey());
-					squadraToReturn.setNome(currentEntity.getValue());
-				}
-			}
-		}
-		return squadraToReturn;
+		return squadreEJB.getSquadraFromMapByNome(nomeSquadraToSearch);
 	}
 
 	public Squadre getSquadraByNome(String nomeSquadraToSearch) {
-		nomeSquadraToSearch = nomeSquadraToSearch.toUpperCase();
-		EntityManager em = getEntityManager();
-		Query query = em.createQuery(QUERY_GET_SQUADRA_BY_NAME);
-		query.setParameter("nomeSquadra", nomeSquadraToSearch.trim().toUpperCase());
-		try {
-			return (Squadre) query.getSingleResult();
-		} catch (NoResultException e) {
-			return null;
-		}
+		return squadreEJB.getSquadraByNome(nomeSquadraToSearch);
 	}
 	
 	public Squadre getSquadraById(int idSquadra) {
-		EntityManager em = getEntityManager();
-		Query query = em.createQuery(QUERY_GET_SQUADRA_BY_ID);
-		query.setParameter("id", idSquadra);
-		try {
-			return (Squadre) query.getSingleResult();
-		} catch (NoResultException e) {
-			return null;
-		}
+		return squadreEJB.getSquadraById(idSquadra);
 	}
 
 }
--- src/main/java/it/zeze/fantaformazioneweb/session/StatisticheList.java
+++ src/main/java/it/zeze/fantaformazioneweb/session/StatisticheList.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import javax.naming.NamingException;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.xml.parsers.ParserConfigurationException;
@@ -26,12 +27,13 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.framework.EntityQuery;
 import org.jboss.seam.log.Log;
 
+import it.zeze.fanta.ejb.util.JNDIUtils;
+import it.zeze.fanta.service.definition.ejb.proxy.seam.StatisticheSeamRemote;
 import it.zeze.fantaformazioneweb.entity.Giocatori;
 import it.zeze.fantaformazioneweb.entity.Giornate;
 import it.zeze.fantaformazioneweb.entity.Statistiche;
 import it.zeze.fantaformazioneweb.entity.StatisticheId;
 import it.zeze.html.cleaner.HtmlCleanerUtil;
-import it.zeze.util.ConfigurationUtil;
 import it.zeze.util.Constants;
 
 @Name("statisticheList")
@@ -70,6 +72,17 @@ public class StatisticheList extends EntityQuery<Statistiche> {
 
 	private List<Statistiche> resultList = new ArrayList<Statistiche>();
 	private List<Statistiche> resumeStatistiche = new ArrayList<Statistiche>();
+	
+	private static StatisticheSeamRemote statisticheEJB;
+	
+	static {
+		try {
+			statisticheEJB = JNDIUtils.getStatisticheEJB();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public void initResultList() {
 		log.info("initResultList");
@@ -250,314 +263,8 @@ public class StatisticheList extends EntityQuery<Statistiche> {
 	}
 
 	public void unmarshallAndSaveFromHtmlFile() {
-		log.info("unmarshallAndSaveFromHtmlFile, entrato");
-		String rootHTMLFiles = ConfigurationUtil.getValue(Constants.CONF_KEY_HTML_ROOT);
-		String nomeFileSquadre = ConfigurationUtil.getValue(Constants.CONF_KEY_HTML_FILE_STATISTICHE_G);
-		// Per tutte le giornate presenti su DB controllo se esiste il relativo
-		// file e lo elaboro
-		String pathCompletoFileSquadre;
-		List<Giornate> listaGiornate = giornateList.getResultList();
-		Giornate currentGiornata;
-		File currentFileGiornata;
-		for (int i = 0; i < listaGiornate.size(); i++) {
-			currentGiornata = listaGiornate.get(i);
-			pathCompletoFileSquadre = rootHTMLFiles + createNomeFileGiornata(nomeFileSquadre, String.valueOf(currentGiornata.getId()));
-			currentFileGiornata = new File(pathCompletoFileSquadre);
-			if (currentFileGiornata.exists()) {
-				try {
-					unmarshallAndSaveFromHtmlFile(currentFileGiornata, String.valueOf(currentGiornata.getId()));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (XPatherException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (XPathExpressionException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ParserConfigurationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (TransformerFactoryConfigurationError e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (TransformerException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		log.info("unmarshallAndSaveFromHtmlFile, uscito");
-	}
-
-	private String createNomeFileGiornata(String nomeFile, String numGiornata) {
-		String fileGiornataToReturn = "";
-		fileGiornataToReturn = StringUtils.replace(nomeFile, Constants.STRING_TO_REPLACE_NOME_FILE_GIORNATE, numGiornata);
-		return fileGiornataToReturn;
-	}
-
-	private void unmarshallAndSaveFromHtmlFile(File fileGiornata, String numGiornata) throws IOException, XPatherException, XPathExpressionException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
-		String currentStagione = sessionInfo.getStagione();
-		currentStagione = giornateList.getStagione(currentStagione);
-		int idGiornata = giornateList.getIdGiornata(Integer.valueOf(numGiornata), currentStagione);
-		if (!statGiaInserite(idGiornata)) {
-			try {
-				// Vecchio HTML
-				log.info("Elaboro statistiche della giornata [" + numGiornata + "] della stagione [" + currentStagione + "]");
-				List<TagNode> listaTabelleVotiPerSquadra = HtmlCleanerUtil.getListOfElementsByXPathFromFile(fileGiornata.getAbsolutePath(), "//div[@id='allvotes']");
-				if (listaTabelleVotiPerSquadra == null || listaTabelleVotiPerSquadra.isEmpty()){
-					throw new Exception("Rilancio per fare parsing con nuovo HTML");
-				}
-				TagNode currentNodeSquadra;
-				String currentNomeSquadra;
-				List<TagNode> listaGiocatori;
-				TagNode currentNodeGiocatore;
-				TagNode currentNodeGiocatoreNome;
-				String currentGiocatoreNome;
-				TagNode currentNodeGiocatoreRuolo;
-				String currentGiocatoreRuolo;
-				List<TagNode> listaColonneGiocatore;
-				TagNode currentColonnaGiocatore;
-				int currentEspulso;
-				int currentAmmonito;
-				String currentGoalFatto;
-				String currentGoalSuRigore;
-				String currentGoalSubito;
-				String currentRigoreParato;
-				String currentRigoreSbagliato;
-				String currentAutorete;
-				String currentAssist;
-				String currentMediaVoto;
-
-				for (int i = 0; i < listaTabelleVotiPerSquadra.size(); i++) {
-					currentNodeSquadra = listaTabelleVotiPerSquadra.get(i);
-					// Recupero il nome della squadra
-					currentNodeSquadra = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentNodeSquadra, "/div[@id]").get(0);
-					currentNomeSquadra = currentNodeSquadra.getAttributeByName("id");
-
-					listaGiocatori = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentNodeSquadra, "/table/tbody/tr[@class='P']");
-					int numColonnaPartenza = 2;
-					for (int y = 0; y < listaGiocatori.size(); y++) {
-						numColonnaPartenza = 2;
-						currentEspulso = 0;
-						currentAmmonito = 0;
-						currentNodeGiocatore = listaGiocatori.get(y);
-						currentNodeGiocatoreNome = currentNodeGiocatore.findElementByAttValue("class", "n", false, false);
-						currentNodeGiocatoreNome = currentNodeGiocatoreNome.findElementByName("a", false);
-						currentGiocatoreNome = currentNodeGiocatoreNome.getText().toString();
-						currentNodeGiocatoreRuolo = currentNodeGiocatore.findElementByAttValue("class", "r", false, false);
-						currentGiocatoreRuolo = currentNodeGiocatoreRuolo.getText().toString();
-						// Salva solo i giocatori non gli allenatori (ruolo = M)
-						if (!currentGiocatoreRuolo.equalsIgnoreCase("M")) {
-							listaColonneGiocatore = currentNodeGiocatore.getElementListByName("td", true);
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentMediaVoto = currentColonnaGiocatore.getText().toString();
-							if (HtmlCleanerUtil.nodeContainsAttribute(currentColonnaGiocatore, "class", "vesp")) {
-								currentEspulso = currentEspulso + 1;
-							} else if (HtmlCleanerUtil.nodeContainsAttribute(currentColonnaGiocatore, "class", "vamm")) {
-								currentAmmonito = currentAmmonito + 1;
-							}
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentGoalFatto = currentColonnaGiocatore.getText().toString();
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentGoalSuRigore = currentColonnaGiocatore.getText().toString();
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentGoalSubito = currentColonnaGiocatore.getText().toString();
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentRigoreParato = currentColonnaGiocatore.getText().toString();
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentRigoreSbagliato = currentColonnaGiocatore.getText().toString();
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentAutorete = currentColonnaGiocatore.getText().toString();
-							currentColonnaGiocatore = listaColonneGiocatore.get(numColonnaPartenza++);
-							currentAssist = currentColonnaGiocatore.getText().toString();
-
-							saveVotoFromHtmlFile(idGiornata, currentStagione, currentGiocatoreNome, currentNomeSquadra, currentGiocatoreRuolo, currentAmmonito, currentEspulso, currentAssist, currentAutorete, currentGoalFatto, currentGoalSuRigore, currentGoalSubito, currentMediaVoto, currentRigoreParato, currentRigoreSbagliato);
-						}
-
-					}
-				}
-
-			} catch (Exception e) {
-				// Vecchio HTML
-				log.info("Nuovo HTML");
-				unmarshallAndSaveFromHtmlFileNEW(fileGiornata, numGiornata, currentStagione);
-			}
-		} else {
-			log.info("Statistiche della giornata [" + numGiornata + "] gia' inserite");
-		}
-	}
-
-	private void unmarshallAndSaveFromHtmlFileNEW(File fileGiornata, String numGiornata, String stagione) throws IOException, XPatherException, XPathExpressionException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
-		int idGiornata = giornateList.getIdGiornata(Integer.valueOf(numGiornata), stagione);
-		log.info("Elaboro statistiche della giornata [" + numGiornata + "] della stagione [" + stagione + "]");
-		List<TagNode> listTabelleSquadre = HtmlCleanerUtil.getListOfElementsByXPathSpecialFromFile(fileGiornata.getAbsolutePath(), "//div[contains(@class,'tbvoti')]//table");
-		for (TagNode currentNodeVotiSquadra : listTabelleSquadre) {
-			salvaVotiSquadraHtmlNEW(idGiornata, stagione, currentNodeVotiSquadra);
-		}
-	}
-
-	private void salvaVotiSquadraHtmlNEW(int idGiornata, String currentStagione, TagNode currentNodeVotiSquadra) throws IOException, XPatherException, XPathExpressionException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
-		String currentNomeSquadra;
-		String currentGiocatoreNome;
-		String currentGiocatoreRuolo;
-		String currentAssistString;
-		String currentAutoreteString;
-		String currentGoalFattoString;
-		String currentGoalSuRigoreString;
-		String currentGoalSubitoString;
-		String currentRigoreParatoString;
-		String currentRigoreSbagliatoString;
-		String mediaVotoString;
-
-		int currentEspulso;
-		int currentAmmonito;
-
-		currentNomeSquadra = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentNodeVotiSquadra, "//table//thead//h3").get(0).getText().toString();
-		log.info("Squadra [" + currentNomeSquadra + "]");
-		List<TagNode> listaGiocatori = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentNodeVotiSquadra, "//tbody/tr");
-		TagNode currentNodeVoto;
-		List<TagNode> listaControlloCartellini;
-		int tdIndexVotiMI;
-		for (TagNode currentGiocatore : listaGiocatori) {
-			tdIndexVotiMI = 10;
-			currentAmmonito = 0;
-			currentEspulso = 0;
-			currentGiocatoreNome = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[@class='pname']//a").get(0).getText().toString();
-			currentGiocatoreRuolo = HtmlCleanerUtil.getListOfElementsByXPathSpecialFromElement(currentGiocatore, "//span[contains(@class,'role')]").get(0).getText().toString();
-			currentNodeVoto = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0);
-			listaControlloCartellini = HtmlCleanerUtil.getListOfElementsByXPathSpecialFromElement(currentGiocatore, "//span[contains(@class,'trn-rr')]");
-			if (listaControlloCartellini == null || !listaControlloCartellini.isEmpty()) {
-				currentEspulso = currentEspulso + 1;
-			}
-			listaControlloCartellini = HtmlCleanerUtil.getListOfElementsByXPathSpecialFromElement(currentGiocatore, "//span[contains(@class,'trn-ry')]");
-			if (listaControlloCartellini == null || !listaControlloCartellini.isEmpty()) {
-				currentAmmonito = currentAmmonito + 1;
-			}
-			mediaVotoString = currentNodeVoto.getText().toString();
-			currentGoalFattoString = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0).getText().toString();
-			currentGoalSuRigoreString = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0).getText().toString();
-			currentGoalSubitoString = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0).getText().toString();
-			currentRigoreParatoString = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0).getText().toString();
-			currentRigoreSbagliatoString = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0).getText().toString();
-			currentAutoreteString = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0).getText().toString();
-			currentAssistString = HtmlCleanerUtil.getListOfElementsByXPathFromElement(currentGiocatore, "//td[" + tdIndexVotiMI++ + "]").get(0).getText().toString();
-
-			if (!currentGiocatoreRuolo.equalsIgnoreCase("ALL")) {
-				saveVotoFromHtmlFile(idGiornata, currentStagione, currentGiocatoreNome, currentNomeSquadra, currentGiocatoreRuolo, currentAmmonito, currentEspulso, currentAssistString, currentAutoreteString, currentGoalFattoString, currentGoalSuRigoreString, currentGoalSubitoString, mediaVotoString, currentRigoreParatoString, currentRigoreSbagliatoString);
-			}
-
-		}
-	}
-
-	private void saveVotoFromHtmlFile(int idGiornata, String currentStagione, String currentGiocatoreNome, String currentNomeSquadra, String currentGiocatoreRuolo, int currentAmmonito, int currentEspulso, String currentAssist, String currentAutorete, String currentGoalFatto, String currentGoalSuRigore, String currentGoalSubito, String currentMediaVoto, String currentRigoreParato, String currentRigoreSbagliato) {
-		// Recupero il giocatore relativo su DB
-		boolean noLike = true;
-		Giocatori currentGiocatoreDB = giocatoriList.getGiocatoreByNomeSquadraRuolo(currentGiocatoreNome, currentNomeSquadra, currentGiocatoreRuolo, currentStagione, noLike);
-		int espulso = currentEspulso;
-		int ammonito = currentAmmonito;
-		int goalFatto;
-		int goalSuRigore;
-		int goalSubito;
-		int rigoreParato;
-		int rigoreSbagliato;
-		int autorete;
-		int assist;
-		BigDecimal mediaVoto;
-		mediaVoto = new BigDecimal(currentMediaVoto.replace(",", "."));
-		if (currentGoalFatto.contains("-")) {
-			goalFatto = 0;
-		} else {
-			goalFatto = Integer.parseInt(currentGoalFatto);
-		}
-		if (currentGoalSuRigore.contains("-")) {
-			goalSuRigore = 0;
-		} else {
-			goalSuRigore = Integer.parseInt(currentGoalSuRigore);
-		}
-		if (currentGoalSubito.contains("-")) {
-			goalSubito = 0;
-		} else {
-			goalSubito = Integer.parseInt(currentGoalSubito);
-		}
-		if (currentRigoreParato.contains("-")) {
-			rigoreParato = 0;
-		} else {
-			rigoreParato = Integer.parseInt(currentRigoreParato);
-		}
-		if (currentRigoreSbagliato.contains("-")) {
-			rigoreSbagliato = 0;
-		} else {
-			rigoreSbagliato = Integer.parseInt(currentRigoreSbagliato);
-		}
-		if (currentAutorete.contains("-")) {
-			autorete = 0;
-		} else {
-			autorete = Integer.parseInt(currentAutorete);
-		}
-		if (currentAssist.contains("-")) {
-			assist = 0;
-		} else {
-			assist = Integer.parseInt(currentAssist);
-		}
-
-		if (currentGiocatoreDB == null) {
-			log.warn("Giocatore [" + currentGiocatoreNome + "] [" + currentNomeSquadra + "] [" + currentGiocatoreRuolo + "] NON presente nel DB. Procedo con il suo inserimento");
-			giocatoriList.insertOrUpdateGiocatore(currentNomeSquadra, currentGiocatoreNome, currentGiocatoreRuolo, currentStagione, noLike);
-			currentGiocatoreDB = giocatoriList.getGiocatoreByNomeSquadraRuolo(currentGiocatoreNome, currentNomeSquadra, currentGiocatoreRuolo, currentStagione, noLike);
-		}
-		log.info("Inserisco statistiche giocatore [" + currentGiocatoreNome + "] [" + currentNomeSquadra + "] [" + currentGiocatoreRuolo + "]");
-		StatisticheId statisticheId = new StatisticheId();
-		statisticheId.setIdGiocatore(currentGiocatoreDB.getId());
-		statisticheId.setIdGiornata(idGiornata);
-		statisticheId.setAmmonizioni(ammonito);
-		statisticheId.setEspulsioni(espulso);
-		statisticheId.setAssist(assist);
-		statisticheId.setAutoreti(autorete);
-		statisticheId.setGoalFatti(goalFatto);
-		statisticheId.setGoalRigore(goalSuRigore);
-		statisticheId.setGoalSubiti(goalSubito);
-		statisticheId.setMediaVoto(mediaVoto);
-		BigDecimal currentMediaFantaVoto = calcolaFantaVoto(mediaVoto, ammonito, espulso, assist, autorete, goalFatto, goalSuRigore, rigoreSbagliato, goalSubito, rigoreParato);
-		statisticheId.setMediaVotoFm(currentMediaFantaVoto);
-		statisticheId.setRigoriParati(rigoreParato);
-		statisticheId.setRigoriSbagliati(rigoreSbagliato);
-
-		statisticheHome.clearInstance();
-		Statistiche statisticheToInsert = new Statistiche();
-		statisticheToInsert.setId(statisticheId);
-		statisticheHome.setInstance(statisticheToInsert);
-		statisticheHome.persist();
-	}
-
-	private BigDecimal calcolaFantaVoto(BigDecimal mediaVoto, int ammonito, int espulso, int assist, int autoreti, int goalFatti, int rigoreFatto, int rigoreSbagliato, int rigoreSubito, int rigoreParato) {
-		BigDecimal fantaVoto = mediaVoto;
-		if (ammonito > 0) {
-			fantaVoto = fantaVoto.subtract(new BigDecimal(0.5));
-		}
-		if (espulso > 0) {
-			fantaVoto = fantaVoto.subtract(BigDecimal.ONE);
-		}
-		fantaVoto = fantaVoto.add(new BigDecimal(assist));
-		if (autoreti > 0) {
-			fantaVoto = fantaVoto.subtract(new BigDecimal(autoreti * 2));
-		}
-		if (goalFatti > 0) {
-			fantaVoto = fantaVoto.add(new BigDecimal(goalFatti * 3));
-		}
-		if (rigoreFatto > 0) {
-			fantaVoto = fantaVoto.add(new BigDecimal(rigoreFatto * 3));
-		}
-		if (rigoreSbagliato > 0) {
-			fantaVoto = fantaVoto.subtract(new BigDecimal(rigoreSbagliato * 3));
-		}
-		if (rigoreSubito > 0) {
-			fantaVoto = fantaVoto.subtract(new BigDecimal(rigoreSubito * 1));
-		}
-		if (rigoreParato > 0) {
-			fantaVoto = fantaVoto.add(new BigDecimal(rigoreParato * 3));
-		}
-		return fantaVoto;
+		String stagione = sessionInfo.getStagione();
+		statisticheEJB.unmarshallAndSaveFromHtmlFile(stagione);
 	}
 
 	public Statistiche getStatisticheIdGiocatoreIdGiornata(int idGiocatore, int idGiornata) {
@@ -601,17 +308,6 @@ public class StatisticheList extends EntityQuery<Statistiche> {
 			log.error("Nessun risultato tovato con idGiocatore [" + idGiocatore + "] e stagione [" + stagioneParse + "]");
 		}
 		return toReturn;
-	}
-
-	private boolean statGiaInserite(int idGiornata) {
-		boolean giaInserita = false;
-		Query query = getEntityManager().createQuery(SELECT_COUNT_BY_ID_GIORNATA);
-		query.setParameter("idGiornata", idGiornata);
-		long count = (Long) query.getSingleResult();
-		if (count > 0) {
-			giaInserita = true;
-		}
-		return giaInserita;
 	}
 
 	public void setLastStagione() {
