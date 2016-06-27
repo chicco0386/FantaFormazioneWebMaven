7641f5b4375439570862fcc951158dc37e236794
enrico <enrico@ic1400833>
2016-06-27 10:05:55 +0200

    Funziona il probabili formazioni

--- src/main/java/it/zeze/fantaformazioneweb/session/ProbabiliFormazioniList.java
+++ src/main/java/it/zeze/fantaformazioneweb/session/ProbabiliFormazioniList.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.naming.NamingException;
-import javax.persistence.Query;
 
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
@@ -19,7 +18,6 @@ import it.zeze.fantaformazioneweb.entity.Giornate;
 import it.zeze.fantaformazioneweb.entity.ProbabiliFormazioni;
 import it.zeze.fantaformazioneweb.entity.ProbabiliFormazioniId;
 import it.zeze.fantaformazioneweb.entity.wrapper.ProbabiliFormazioniWrap;
-import it.zeze.util.Constants;
 
 @Name("probabiliFormazioniList")
 public class ProbabiliFormazioniList extends EntityQuery<ProbabiliFormazioni> {
@@ -50,10 +48,6 @@ public class ProbabiliFormazioniList extends EntityQuery<ProbabiliFormazioni> {
 
 	private static final String[] RESTRICTIONS = { "probabiliFormazioni.id.idGiornate = #{probabiliFormazioniList.probabiliFormazioni.id.idGiornate}", "probabiliFormazioni.id.idUtentiFormazioni = #{probabiliFormazioniList.probabiliFormazioni.id.idUtentiFormazioni}" };
 
-	private static final String SELECT_BY_ID_GIORNATA_ID_UTENTIFORMAZIONE = "select probabiliFormazioni from ProbabiliFormazioni probabiliFormazioni where probabiliFormazioni.id.idGiornate=:idGiornata and probabiliFormazioni.id.idUtentiFormazioni=:idUtentiFormazione order by probabiliFormazioni.giocatori.ruolo desc";
-	private static final String DELETE_BY_ID_GIORNATA_ID_UTENTIFORMAZIONE = "delete from ProbabiliFormazioni probabiliFormazioni where probabiliFormazioni.id.idGiornate=:idGiornata and probabiliFormazioni.id.idUtentiFormazioni=:idUtentiFormazione";
-	private static final String DELETE_BY_ID_UTENTI_FORMAZIONI = "DELETE FROM ProbabiliFormazioni WHERE id.idUtentiFormazioni=:idUtentiFormazioni";
-
 	private ProbabiliFormazioni probabiliFormazioni;
 
 	private List<ProbabiliFormazioni> resultList;
@@ -63,21 +57,14 @@ public class ProbabiliFormazioniList extends EntityQuery<ProbabiliFormazioni> {
 		probabiliFormazioni.setGiornate(new Giornate());
 		probabiliFormazioni.setId(new ProbabiliFormazioniId());
 		setEjbql(EJBQL);
-		// probabiliFormazioni.getId().setIdGiornate(giornateList.getIdGiornata(probabiliFormazioni.getGiornate().getNumeroGiornata(),
-		// probabiliFormazioni.getGiornate().getStagione()));
 		setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS));
 		refresh();
-		// setMaxResults(25);
-//		setOrderColumn("giocatori.ruolo");
-//		setOrderDirection("desc");
 	}
 
 	public List<ProbabiliFormazioni> getRisultati(int idUtentiFormazione, String stagione, int numeroGiornata) {
 		List<ProbabiliFormazioni> toReturn = new ArrayList<ProbabiliFormazioni>();
 		List<ProbabiliFormazioniWrap> ejbResp = probabiliFormazioniEJB.getRisultati(idUtentiFormazione, stagione, numeroGiornata);
-		for (ProbabiliFormazioniWrap current : ejbResp){
-			toReturn.add(current.unwrap());
-		}
+		toReturn = unwrapEJBResp(ejbResp);
 		if (toReturn != null && !toReturn.isEmpty()){
 			int idGiornata = toReturn.get(0).getGiornate().getId();
 			probabiliFormazioni.getId().setIdGiornate(idGiornata);
@@ -91,38 +78,22 @@ public class ProbabiliFormazioniList extends EntityQuery<ProbabiliFormazioni> {
 	}
 
 	public List<ProbabiliFormazioni> getProbFormazioniByGiornataUtentiFormazione(int idGiornata, int idUtentiFormazione) {
-		Query query = getEntityManager().createQuery(SELECT_BY_ID_GIORNATA_ID_UTENTIFORMAZIONE);
-		query.setParameter("idGiornata", idGiornata);
-		query.setParameter("idUtentiFormazione", idUtentiFormazione);
-		List<ProbabiliFormazioni> toReturn = query.getResultList();
-		log.info("------ " + toReturn.size());
+		List<ProbabiliFormazioni> toReturn = new ArrayList<ProbabiliFormazioni>();
+		List<ProbabiliFormazioniWrap> ejbResp = probabiliFormazioniEJB.getProbFormazioniByGiornataUtentiFormazione(idGiornata, idUtentiFormazione);
+		toReturn = unwrapEJBResp(ejbResp);
 		return toReturn;
 	}
 
 	public void deleteProbFormazioniByGiornataUtentiFormazione(int idGiornata, int idUtentiFormazione) {
-		Query query = getEntityManager().createQuery(DELETE_BY_ID_GIORNATA_ID_UTENTIFORMAZIONE);
-		query.setParameter("idGiornata", idGiornata);
-		query.setParameter("idUtentiFormazione", idUtentiFormazione);
-		log.info("Cancellati [" + query.executeUpdate() + "] record da ProbabiliFormazioni");
+		probabiliFormazioniEJB.deleteProbFormazioniByGiornataUtentiFormazione(idGiornata, idUtentiFormazione);
 	}
 	
 	public void deleteProbFormazioniByUtentiFormazione(int idUtentiFormazione) {
-		Query query = getEntityManager().createQuery(DELETE_BY_ID_UTENTI_FORMAZIONI);
-		query.setParameter("idUtentiFormazioni", idUtentiFormazione);
-		log.info("Cancellati [" + query.executeUpdate() + "] record da ProbabiliFormazioni");
+		probabiliFormazioniEJB.deleteProbFormazioniByUtentiFormazione(idUtentiFormazione);
 	}
 
 	public void insertProbFormazione(int idGiornata, int idUtentiFormazione, int idGiocatore, int probTit, int probPanc, String note) {
-		log.debug("Inserisco probabile formazione idGiornata [" + idGiornata + "] idUtentiFormazione [" + idUtentiFormazione + "] idGiocatore [" + idGiocatore + "] probTit [" + probTit + "] probPanc [" + probPanc + "] note [" + note + "]");
-		ProbabiliFormazioniId probabiliFormazioniId = null;
-		if (note != null) {
-			probabiliFormazioniId = new ProbabiliFormazioniId(idGiornata, idUtentiFormazione, idGiocatore, probTit, probPanc, note);
-		} else {
-			probabiliFormazioniId = new ProbabiliFormazioniId(idGiornata, idUtentiFormazione, idGiocatore, probTit, probPanc);
-		}
-		ProbabiliFormazioni probabiliFormazioni = new ProbabiliFormazioni();
-		probabiliFormazioni.setId(probabiliFormazioniId);
-		getEntityManager().persist(probabiliFormazioni);
+		probabiliFormazioniEJB.insertProbFormazione(idGiornata, idUtentiFormazione, idGiocatore, probTit, probPanc, note);
 	}
 
 	public List<ProbabiliFormazioni> getResultList() {
@@ -140,18 +111,7 @@ public class ProbabiliFormazioniList extends EntityQuery<ProbabiliFormazioni> {
 	 * @return
 	 */
 	public boolean isFantaGazzettaSource(int probabilita) {
-		boolean fantaGazzetta = false;
-		if (probabilita != 0) {
-			if (probabilita % Constants.PROB_FANTA_GAZZETTA == 0) {
-				fantaGazzetta = true;
-			} else {
-				int probTemp = probabilita - Constants.PROB_GAZZETTA;
-				if (probTemp != 0 && probTemp % Constants.PROB_FANTA_GAZZETTA == 0) {
-					fantaGazzetta = true;
-				}
-			}
-		}
-		return fantaGazzetta;
+		return probabiliFormazioniEJB.isFantaGazzettaSource(probabilita);
 	}
 
 	/**
@@ -161,17 +121,14 @@ public class ProbabiliFormazioniList extends EntityQuery<ProbabiliFormazioni> {
 	 * @return
 	 */
 	public boolean isGazzettaSource(int probabilita) {
-		boolean gazzetta = false;
-		if (probabilita != 0) {
-			if (probabilita % Constants.PROB_GAZZETTA == 0) {
-				gazzetta = true;
-			} else {
-				int probTemp = probabilita - Constants.PROB_FANTA_GAZZETTA;
-				if (probTemp != 0 && probTemp % Constants.PROB_GAZZETTA == 0) {
-					gazzetta = true;
-				}
-			}
+		return probabiliFormazioniEJB.isGazzettaSource(probabilita);
+	}
+	
+	private List<ProbabiliFormazioni> unwrapEJBResp(List<ProbabiliFormazioniWrap> ejbResp){
+		List<ProbabiliFormazioni> toReturn = new ArrayList<ProbabiliFormazioni>();
+		for (ProbabiliFormazioniWrap current : ejbResp){
+			toReturn.add(current.unwrap());
 		}
-		return gazzetta;
+		return toReturn;
 	}
 }
